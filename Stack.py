804e03f8384312302c9dea319f499f3e94d16433
usman muhammad <abubakargaming@gmail.com>
2026-09-14 18:25:01 +0100

    Completed Valid Parenthesis

--- Stack.py
+++ Stack.py
@@ -1,3 +1,25 @@
 #Valid Parentheses
+s1 = "[]"
+s2 = "([{}])"
+s3 = "[(])"
+s4 = "(){}}{"
+
+
+
 def isValid(s: str) -> bool:
-    return True
+    hashmap = {"[" : "]", "{" : "}", "(" : ")"}
+    open  = set(["[", "{", "("])
+    stack = []
+    for i in range(len(s)):
+        if s[i] in open:
+            stack.append(s[i])
+        elif len(stack) == 0:
+            return False
+        elif s[i] != hashmap[stack[len(stack) - 1]]:
+            return False
+        else:
+            stack.pop() 
+        print(stack)
+    return len(stack) == 0 
+
+print(isValid(s4))

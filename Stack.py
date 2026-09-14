55176b20ca0433a7a5b0225f822b5480f7deb8e4
usman muhammad <abubakargaming@gmail.com>
2026-09-14 22:48:11 +0100

    Completed Reverse Polish Notation

--- Stack.py
+++ Stack.py
@@ -1,11 +1,6 @@
+import operator #For Revese Polish Notation 
+
 #Valid Parentheses
-s1 = "[]"
-s2 = "([{}])"
-s3 = "[(])"
-s4 = "(){}}{"
-
-
-
 def isValid(s: str) -> bool:
     hashmap = {"[" : "]", "{" : "}", "(" : ")"}
     open  = set(["[", "{", "("])
@@ -22,4 +17,49 @@ def isValid(s: str) -> bool:
         print(stack)
     return len(stack) == 0 
 
-print(isValid(s4))
+#Min Stack
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.minstack) == 0:
+            self.minstack.append(val)
+        else:
+            mini = min(val, self.minstack[-1])
+            self.minstack.append(mini)
+            
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+
+
+t1 = ["2","1","+","3","*"]
+# Evaluate Reverse Polish Notation
+def EvalRPN(tokens: list[str]) -> int:
+    operators = {
+        "+" : operator.add,
+        "-" : operator.sub,
+        "*" : operator.mul,
+        "/" : operator.truediv
+    }
+    stack = []
+    for i in range(len(tokens)):
+        if tokens[i] not in operators:
+            stack.append(tokens[i])
+        else: 
+            operand1 = int(stack.pop())
+            operand2 = int(stack.pop())
+            stack.append(operators[tokens[i]](operand2, operand1))
+    return int(stack.pop())
+EvalRPN(t1)     
